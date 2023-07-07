b036a9faa8ff34c2e969b8a640fdd08bc24edcc8
NirajChaudhary <chaudharyniraj0345@gmail.com>
2023-07-08 00:17:11 +0545

    nothing

--- manageproduct.php
+++ manageproduct.php
@@ -5,17 +5,51 @@
         header('location:login.php');
     }
 ?>
+<?php
+	include 'includes/connection.php';
 
+    if(isset($_POST['add'])){
+      $name = $_POST['name'];
+      $bought = $_POST['bought'];
+      $sold = $_POST['sold'];
+      $stock = $_POST['stock'];
+
+      $imagename = $_FILES['image']['name'];
+      $tmpname = $_FILES['image']['tmp_name'];
+      $img_type = strtolower(pathinfo($imagename,PATHINFO_EXTENSION));//gets extension of the image
+      $destination = "./images/".$imagename;//Saves image
+      $allow_type= array('png','jpeg','jpg');//restriction of png jpeg and jpg
+      $imagesize = $_FILES['image']['size'];//file size
+
+
+      //image validation
+      if(in_array($img_type, $allow_type)){//checks image extension
+        if($imagesize <= 2000000){//checks image size
+          move_uploaded_file($tmpname, $destination);//moves the image to project image folder
+            $insertquery = "INSERT INTO product(product_name, product_bought, product_sold, product_stock, product_image) VALUES('{$name}','{$bought}','{$sold}','{$stock}','$imagename')";
+            $query = mysqli_query($con,$insertquery);
+        }
+        else{
+          echo "Size exceded";
+        }
+      }
+      else{
+        echo "file type Not allowed";
+      }
+
+    }
+?>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
 	<link rel="stylesheet" href="assets/css/style.css">
+	<link rel="stylesheet" href="assets/css/add.css">
 	<title>Stationery Management System</title>
 </head>
 <body>
-	
+	<span class="overlay"></span>
     <!-- SIDEBAR -->
 <section id="sidebar"> 
     <?php 
@@ -36,6 +70,49 @@
         <li><a href="product.php" class="active">Product</a></li>
     </ul>
     <div class="content">
+    <section class="modal">
+            <button class="modal__button" id="open-modal">
+                Add Product
+            </button>
+
+            <div class="modal__container" id="modal-container">
+                <div class="modal__content">
+                    <div class="modal__close close-modal" title="Close">
+                        <i class='bx bx-x'></i>
+                    </div>
+                    <div class="add-form">
+                        <div class="form-container">
+                        <div class="title">Add Product</div>
+                        <form action="" method="POST" enctype="multipart/form-data">
+                            <div class="row">
+                                <label for="">Product Name</label>
+                                <input type="text" name="name" required>
+                            </div>
+                            <div class="row">
+                            <label for="">Product Image</label>
+                                <input type="file"  name="image">
+                            </div>
+                            <div class="row">
+                            <label for="">Product Bought</label>
+                                <input type="number" name="bought" required>
+                            </div>
+                            <div class="row">
+                            <label for="">Product Sold</label>
+                                <input type="number" name="sold" required>
+                            </div>
+                            <div class="row">
+                            <label for="">Stock</label>
+                                <input type="number" name="stock" required>
+                            </div>
+                            <div class="row button">
+                                <input type="submit" value="Add" name="add">
+                            </div>
+                        </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
         <div class="table">
             <section class="table_body">
                 <table>
@@ -86,11 +163,9 @@
     </div>
 </main>
 <!-- Main Content -->
-
-
-
 </section>
     <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
     <script src="assets\js\dashboard.js"></script>
+    <!-- <script src="assets\js\modal.js"></script> -->
 </body>
 </html>    
